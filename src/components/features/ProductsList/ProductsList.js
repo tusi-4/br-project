6c95c8aaa9b20ccbf3e11b80f7dac8aa6c4ab387
Marta Wojdat <marjow4@gmail.com>
2022-01-20 19:27:27 +0100

    Safety commit

--- src/components/features/ProductsList/ProductsList.js
+++ src/components/features/ProductsList/ProductsList.js
@@ -9,74 +9,18 @@ import { getAllProducts, fetchAllProducts } from '../../../redux/productsRedux.j
 
 import { Link } from 'react-router-dom';
 
-//import { Product } from '../Product/Product';
-
-// const demoContent = [
-//   {
-//     _id: '61e732ba238fc0f164fb3de1',
-//     name: 'Berry Me!',
-//     images: [
-//       'https://images.pexels.com/photos/8679161/pexels-photo-8679161.jpeg',
-//       'https://images.pexels.com/photos/8679152/pexels-photo-8679152.jpeg',
-//       'https://images.pexels.com/photos/8679162/pexels-photo-8679162.jpeg',
-//       'https://images.pexels.com/photos/8679167/pexels-photo-8679167.jpeg',
-//     ],
-//     minprice: 19,
-//     description: 'Lorem ipsum dolor sit amet enim. Etiam ullamcorper. Suspendisse a pellentesque dui, non felis. Maecenas malesuada elit lectus felis, malesuada ultricies. Curabitur et ligula. Ut molestie a, ultricies porta urna.',
-//     ingredients: ['d', 'dd'],
-//   },
-
-//   {
-//     _id: '61e733a6238fc0f164fb3de2',
-//     name: 'Oui, Kiwi!',
-//     images: [
-//       'https://images.pexels.com/photos/8679178/pexels-photo-8679178.jpeg',
-//       'https://images.pexels.com/photos/8679168/pexels-photo-8679168.jpeg',
-//     ],
-//     minprice: 17,
-//     description: 'Vestibulum commodo volutpat a, convallis ac, laoreet enim. Phasellus fermentum in, dolor. Pellentesque facilisis. Nulla imperdiet sit amet magna.',
-//     ingredients: [
-//       'c', 'cc', 'ccc',
-//     ],
-//   },
-
-//   {
-//     _id: '61e73461238fc0f164fb3de3',
-//     name: 'Winter mood',
-//     images: [
-//       'https://images.pexels.com/photos/8679129/pexels-photo-8679129.jpeg',
-//       'https://images.pexels.com/photos/8679123/pexels-photo-8679123.jpeg',
-//       'https://images.pexels.com/photos/8679132/pexels-photo-8679132.jpeg',
-//     ],
-//     minprice: 22,
-//     description: 'Vestibulum dapibus, mauris nec malesuada fames ac turpis velit, rhoncus eu, luctus et interdum adipiscing wisi. Aliquam erat ac ipsum. Integer aliquam purus. Quisque lorem tortor fringilla sed, vestibulum id, eleifend justo vel bibendum sapien massa ac turpis faucibus orci luctus non, consectetuer lobortis quis, varius in, purus.',
-//     ingredients: [
-//       'b', 'bb', 'bbb', 'bbbb',
-//     ],
-//   },
-
-//   {
-//     _id: '61e735db238fc0f164fb3de4',
-//     name: 'Vitamin T&C',
-//     images: [
-//       'https://images.pexels.com/photos/8679544/pexels-photo-8679544.jpeg',
-//       'https://images.pexels.com/photos/8679549/pexels-photo-8679549.jpeg',
-//       'https://images.pexels.com/photos/8679546/pexels-photo-8679546.jpeg',
-//       'https://images.pexels.com/photos/8679541/pexels-photo-8679541.jpeg',
-//     ],
-//     minprice: 15,
-//     description: 'Integer ultrices posuere cubilia Curae, Nulla ipsum dolor lacus, suscipit adipiscing.',
-//     ingredients: [
-//       'a', 'aa', 'aaa', 'aaaa', 'aaaaa',
-//     ],
-//   },
-// ];
+import ShoppingCartOutlinedIcon from '@material-ui/icons/ShoppingCartOutlined';
 
 const Component = ({className, products, fetchProducts}) => {
   fetchProducts();
 
   return (
     <div id="products-list" className={clsx(className, styles.productlistRoot)}>
+      <div className={clsx(className, styles.productlistCart)}>
+        <Link className={clsx(className, styles.productMenulink)} to="/cart">
+          <ShoppingCartOutlinedIcon className={clsx(className, styles.productlistIcon)} />
+        </Link>
+      </div>
       <h2 className={clsx(className, styles.productlistH2)}>beverages</h2>
       <div className={clsx(className, styles.productlistGallerywrapper)}>
         {products && products.length > 0 && products.map(product => (
--- src/components/views/Product/Product.js
+++ src/components/views/Product/Product.js
@@ -10,128 +10,6 @@ import { getOneProduct, fetchProductById } from '../../../redux/productsRedux';
 
 import { Link } from 'react-router-dom';
 
-// const demoContent = [
-//   {
-//     _id: '61e732ba238fc0f164fb3de1',
-//     name: 'Berry Me!',
-//     images: [
-//       'https://images.pexels.com/photos/8679161/pexels-photo-8679161.jpeg',
-//       'https://images.pexels.com/photos/8679152/pexels-photo-8679152.jpeg',
-//       'https://images.pexels.com/photos/8679162/pexels-photo-8679162.jpeg',
-//       'https://images.pexels.com/photos/8679167/pexels-photo-8679167.jpeg',
-//     ],
-//     minprice: 19,
-//     description: 'Lorem ipsum dolor sit amet enim. Etiam ullamcorper. Suspendisse a pellentesque dui, non felis. Maecenas malesuada elit lectus felis, malesuada ultricies. Curabitur et ligula. Ut molestie a, ultricies porta urna.',
-//     ingredients: ['d', 'dd'],
-//   },
-
-//   {
-//     _id: '61e732ba238fc0f164fb3de1',
-//     name: 'Berry Me!',
-//     images: [
-//       'https://images.pexels.com/photos/8679161/pexels-photo-8679161.jpeg',
-//       'https://images.pexels.com/photos/8679152/pexels-photo-8679152.jpeg',
-//       'https://images.pexels.com/photos/8679162/pexels-photo-8679162.jpeg',
-//       'https://images.pexels.com/photos/8679167/pexels-photo-8679167.jpeg',
-//     ],
-//     minprice: 19,
-//     description: 'Lorem ipsum dolor sit amet enim. Etiam ullamcorper. Suspendisse a pellentesque dui, non felis. Maecenas malesuada elit lectus felis, malesuada ultricies. Curabitur et ligula. Ut molestie a, ultricies porta urna.',
-//     ingredients: ['d', 'dd'],
-//   },
-
-//   {
-//     _id: '61e732ba238fc0f164fb3de1',
-//     name: 'Berry Me!',
-//     images: [
-//       'https://images.pexels.com/photos/8679161/pexels-photo-8679161.jpeg',
-//       'https://images.pexels.com/photos/8679152/pexels-photo-8679152.jpeg',
-//       'https://images.pexels.com/photos/8679162/pexels-photo-8679162.jpeg',
-//       'https://images.pexels.com/photos/8679167/pexels-photo-8679167.jpeg',
-//     ],
-//     minprice: 19,
-//     description: 'Lorem ipsum dolor sit amet enim. Etiam ullamcorper. Suspendisse a pellentesque dui, non felis. Maecenas malesuada elit lectus felis, malesuada ultricies. Curabitur et ligula. Ut molestie a, ultricies porta urna.',
-//     ingredients: ['d', 'dd'],
-//   },
-
-//   {
-//     _id: '61e732ba238fc0f164fb3de1',
-//     name: 'Berry Me!',
-//     images: [
-//       'https://images.pexels.com/photos/8679161/pexels-photo-8679161.jpeg',
-//       'https://images.pexels.com/photos/8679152/pexels-photo-8679152.jpeg',
-//       'https://images.pexels.com/photos/8679162/pexels-photo-8679162.jpeg',
-//       'https://images.pexels.com/photos/8679167/pexels-photo-8679167.jpeg',
-//     ],
-//     minprice: 19,
-//     description: 'Lorem ipsum dolor sit amet enim. Etiam ullamcorper. Suspendisse a pellentesque dui, non felis. Maecenas malesuada elit lectus felis, malesuada ultricies. Curabitur et ligula. Ut molestie a, ultricies porta urna.',
-//     ingredients: ['d', 'dd'],
-//   },
-// ];
-
-// const Component = ({className, product, fetchOne }) => {
-//   useEffect(() => {
-//     fetchOne();
-//   }, [fetchOne]);
-
-//   return (
-//     <div className={clsx(className, styles.root)}>
-//       <div className={clsx(className, styles.menuwrapper)}>
-//         <Link className={clsx(className, styles.menulink)} to="/">Homepage</Link>
-//         <Link className={clsx(className, styles.menulink)} to="/cart">Cart</Link>
-//       </div>
-//       {product.map(item => (
-//         <div key={item._id} className={clsx(className, styles.productwrapper)}>
-//           <div className={clsx(className, styles.producttext)}>
-//             <div className={clsx(className, styles.info)}>
-//               <h3>{item.name}</h3>
-//               <h4>{item.minprice}$</h4>
-//               <h5>Ingredients:</h5>
-//               <ul>
-//                 {item.ingredients.map(ingredient => (
-//                   <li key={ingredient}>{ingredient}</li>
-//                 ))}
-//               </ul>
-//             </div>
-//             <p className={clsx(className, styles.description)}>
-//               {item.description}
-//             </p>
-//           </div>
-//           <div className={clsx(className, styles.productgallery)}>
-//             {item.images.map(image => (
-//               <img className={clsx(className, styles.productimg)} alt={item.name} key={image} src={image} />
-//             ))}
-//           </div>
-//         </div>
-//       ))}
-//     </div>
-//   );
-// };
-
-// Component.propTypes = {
-//   className: PropTypes.string,
-//   product: PropTypes.object,
-//   fetchOne: PropTypes.func,
-// };
-
-// const mapStateToProps = (state, props) => {
-//   const product = getOneProduct(state, props.match.params.id);
-
-//   return ({
-//     product,
-//   });
-// };
-
-// const mapDispatchToProps = (dispatch, props) => ({
-//   fetchOne: () => dispatch(fetchProductById(props.match.params.id)),
-// });
-
-// const Container = connect(mapStateToProps, mapDispatchToProps)(Component);
-
-// export {
-//   Container as Product,
-//   Component as ProductComponent,
-// };
-
 const Component = ({className, product, fetchOne }) => {
   useEffect(() => {
     fetchOne();
@@ -144,6 +22,7 @@ const Component = ({className, product, fetchOne }) => {
         <Link className={clsx(className, styles.productMenulink)} to="/cart">Cart</Link>
       </div>
       <div key={product._id} className={clsx(className, styles.productProductwrapper)}>
+        <Link className={clsx(className, styles.productAddlink)} to="/form">Add to cart</Link>
         <div className={clsx(className, styles.productProducttext)}>
           <div className={clsx(className, styles.productInfo)}>
             <h3 className={clsx(className, styles.productH3)}>{product.name}</h3>
@@ -164,6 +43,7 @@ const Component = ({className, product, fetchOne }) => {
             <img className={clsx(className, styles.productProductimg)} alt={product.name} key={image} src={image} />
           ))}
         </div>
+        <Link className={clsx(className, styles.productAddlink)} to="/form">Add to cart</Link>
       </div>
     </div>
   );
