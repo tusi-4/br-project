34ef3ce71f92c7a2680208bd312a936668439941
Marta Wojdat <marjow4@gmail.com>
2022-01-20 22:06:30 +0100

    Add text for About

--- src/components/features/About/About.js
+++ src/components/features/About/About.js
@@ -8,7 +8,7 @@ const Component = (className) => (
   <div id="about-beverider" className={clsx(className, styles.aboutRoot)}>
     <h2 className={clsx(className, styles.aboutH2)}>about</h2>
     <p className={clsx(className, styles.about)}>
-        Lorem ipsum dolor sit amet enim. Etiam ullamcorper. Suspendisse a pellentesque dui, non felis. Maecenas malesuada elit lectus felis, malesuada ultricies. Curabitur et ligula. Ut molestie a, ultricies porta urna. Vestibulum commodo volutpat a, convallis ac, laoreet enim. Phasellus fermentum in, dolor. Pellentesque facilisis. Nulla imperdiet sit amet magna. Vestibulum dapibus, mauris nec malesuada fames ac turpis velit, rhoncus eu, luctus et interdum adipiscing wisi. Aliquam erat ac ipsum. Integer aliquam purus.
+        Unexpected guests? A party without the need of dressing up and going out? Your pizzeria doesn&apos;t offer anything fancy to drink?<br />YOU can solve it with minimal effort!<br />Just choose from our colorful range of all-kind beverages, make an order and we&apos;ll take care of the rest. Our &quot;bartender on wheels&quot; will deliver to you all the necessary (and extra!) ingredients and recipes. Just mix and have fun!
     </p>
   </div>
 );
