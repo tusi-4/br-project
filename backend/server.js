a07985505e9c25f97b2be4736b7a26086101eed1
Marta Wojdat <marjow4@gmail.com>
2022-01-26 19:41:11 +0100

    Add config vars

--- backend/server.js
+++ backend/server.js
@@ -27,11 +27,9 @@ app.use('*', (req, res) => {
 const NODE_ENV = process.env.NODE_ENV;
 let dbUri = '';
 
-if(NODE_ENV === 'production') dbUri = 'mongodb+srv://tusi:babajaga@clusterbr.bv9km.mongodb.net/br-project?retryWrites=true&w=majority';
+if(NODE_ENV === 'production') dbUri = `${process.env.protected12476}`;
 else if(NODE_ENV === 'test') dbUri = 'mongodb://localhost:27017/br-project';
 else dbUri = 'mongodb://localhost:27017/br-project';
-
-dbUri = 'mongodb+srv://tusi:babajaga@clusterbr.bv9km.mongodb.net/br-project?retryWrites=true&w=majority';
 
 mongoose.connect(dbUri, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
